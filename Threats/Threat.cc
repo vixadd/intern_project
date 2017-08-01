69ec0fe673de91526ce5cdce7f8f8b76dd002f5e
vixadd <dkroell@acm.org>
2017-08-01 08:02:51 +0200

    Messed up threat

--- Threats/Threat.cc
+++ Threats/Threat.cc
@@ -1,22 +1,3 @@
 #include <iostream>   // std::cout
 #include <string>     // std::string, std::to_string
 
-class Threat
-{
-private:
-	string m_szName;
-	int m_id;
-	
-public:
-	Threat(string szName, int id)
-	{
-		m_szName = szName;
-		m_id = id;
-	}
-  
-  void toString()
-	{
-		cout << "Last name: " << szLastName << endl;
-		cout << "Year of birth: " << iYearOfBirth << endl;
-	}
-};
